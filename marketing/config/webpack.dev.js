6477079fb6b10926a7c6052564183b619be52705
Ujjwal Goel <ujjwal.goel@publicissapient.com>
2021-05-16 22:05:32 +0530

    added shared config for react and react-dom in container and marketing

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -17,7 +17,8 @@ const devConfig = {
             filename: 'remoteEntry.js',
             exposes: {
                 './MarketingApp' : './src/bootstrap',
-            }
+            },
+            shared: ['react', 'react-dom']
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html'
